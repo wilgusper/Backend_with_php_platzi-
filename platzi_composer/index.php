d4e0df0d40b3cfbe89ede9a0efe8012f8acd4ae0
wilgusper <wgperillac@gmail.com>
2023-11-13 13:12:10 -0500

    funcional composer

---
+++ platzi_composer/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//Se obtiene la configuración inicial del autoload generado por composer dump
+require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * En este caso imprime el resultado de la función sumaDosNumeros() que se declaro en helpers.php
+* archivo que se intancia en el autoload_files
+ */
+echo lower('Hola');
